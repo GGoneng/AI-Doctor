428dd8a39dc51678d0f674d531ba234d115b0793
GGoneng <zxzx0031@naver.com>
2025-08-28 02:02:17 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Vision/Image_Segmentation.ipynb
    	new file:   Vision/XRaySegModules.py

---
+++ Vision/XRaySegModules.py
@@ -0,0 +1,35 @@
+import torch
+
+from torch.utils.data import Dataset, DataLoader
+
+from PIL import Image, ImageDraw
+
+import json
+import os
+
+class XRayDataset(Dataset):
+    def __init__(self, img_path, json_list, transform=None):
+        self.img_path = img_path
+        self.label = json_list
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        item = self.label[idx]
+
+        img = Image.open(self.img_path).convert('L')
+
+        mask = Image.new('L', img.size, 0)
+        draw = ImageDraw.Draw(mask)
+
+        for shape in item["shapes"]:
+            points = [tuple(point) for point in shape["points"]]
+            draw.polygon(points, fill=1)
+
+        if self.transform:
+            img = self.transform(img)
+            mask = self.transform(mask)
+
+        return img, mask
