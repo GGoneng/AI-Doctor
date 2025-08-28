10d24a346bc31f2e9c5c7de4751e480ff7851d12
GGoneng <zxzx0031@naver.com>
2025-08-29 05:25:41 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Vision/Image_Segmentation.ipynb
    	modified:   Vision/XRaySegModules.py
    	modified:   Vision/__pycache__/XRaySegModules.cpython-310.pyc

--- Vision/XRaySegModules.py
+++ Vision/XRaySegModules.py
@@ -1,11 +1,10 @@
 import torch
+import torch.nn as nn
 
 from torch.utils.data import Dataset, DataLoader
 
 from PIL import Image, ImageDraw
 
-import json
-import os
 
 class XRayDataset(Dataset):
     def __init__(self, img_path, json_list, transform=None):
@@ -34,3 +33,68 @@ class XRayDataset(Dataset):
             mask = self.transform(mask)
 
         return img, mask
+
+class Conv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.ReLU()
+        )
+    
+    def forward(self, input):
+        return self.conv(input)
+    
+class Expand(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.up = nn.Sequential(
+            nn.ConvTranspose2d(in_ch, out_ch, 2, stride=2),
+        )
+        self.conv = Conv(in_ch, out_ch)
+
+    def forward(self, input, skip):
+        x = self.up(input)
+        x = torch.cat((x, skip), dim=1)
+        x = self.conv(x)
+
+        return x
+
+class OriginUNet(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        
+        self.encoder1 = Conv(in_ch, 64)
+        self.encoder2 = Conv(64, 128)
+        self.encoder3 = Conv(128, 256)
+        self.encoder4 = Conv(256, 512)
+
+        self.maxpool = nn.MaxPool2d(2)
+
+        self.bottleneck = Conv(512, 1024)
+
+        self.decoder1 = Expand(1024, 512)
+        self.decoder2 = Expand(512, 256)
+        self.decoder3 = Expand(256, 128)
+        self.decoder4 = Expand(128, 64)
+
+        self.output = nn.Conv2d(64, out_ch, 1)
+
+    def forward(self, input):
+        in1 = self.encoder1(input)
+        in2 = self.encoder2(self.maxpool(in1))
+        in3 = self.encoder3(self.maxpool(in2))
+        in4 = self.encoder4(self.maxpool(in3))
+
+        bn = self.bottleneck(self.maxpool(in4))
+
+        out1 = self.decoder1(bn, in4)
+        out2 = self.decoder2(out1, in3)
+        out3 = self.decoder3(out2, in2)
+        out4 = self.decoder4(out3, in1)
+
+        final_output = self.output(out4)
+
+        return final_output
