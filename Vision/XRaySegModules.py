65e685be0bbc9a126efc5898220bd4ac90e13c1d
GGoneng <zxzx0031@naver.com>
2025-08-31 04:16:15 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Vision/Image_Segmentation.ipynb
    	modified:   Vision/XRaySegModules.py
    	modified:   Vision/__pycache__/XRaySegModules.cpython-310.pyc

--- Vision/XRaySegModules.py
+++ Vision/XRaySegModules.py
@@ -1,11 +1,14 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
+from torchmetrics.segmentation import DiceScore
 from torch.utils.data import Dataset, DataLoader
 
 from PIL import Image, ImageDraw
 
 import numpy as np
+
 
 
 class XRayDataset(Dataset):
@@ -27,7 +30,8 @@ class XRayDataset(Dataset):
 
         for shape in item["shapes"]:
             points = [tuple(point) for point in shape["points"]]
-            draw.polygon(points, fill=1)
+            class_id = shape["class"]
+            draw.polygon(points, fill=class_id)
         
         img = np.array(img)
         mask = np.array(mask)
@@ -70,10 +74,10 @@ class Expand(nn.Module):
         return x
 
 class OriginUNet(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, num_classes):
         super().__init__()
         
-        self.encoder1 = Conv(in_ch, 64)
+        self.encoder1 = Conv(1, 64)
         self.encoder2 = Conv(64, 128)
         self.encoder3 = Conv(128, 256)
         self.encoder4 = Conv(256, 512)
@@ -87,9 +91,9 @@ class OriginUNet(nn.Module):
         self.decoder3 = Expand(256, 128)
         self.decoder4 = Expand(128, 64)
 
-        self.output = nn.Conv2d(64, out_ch, 1)
+        self.output = nn.Conv2d(64, num_classes, 1)
 
-        self.dropout = nn.Dropout2D(0.2)
+        self.dropout = nn.Dropout2d(0.2)
 
     def forward(self, input):
         in1 = self.encoder1(input)
@@ -107,4 +111,54 @@ class OriginUNet(nn.Module):
         final_output = self.output(out4)
 
         return final_output
+
+
+class CustomWeightedLoss(nn.Module):
+    def __init__(self, num_cls):
+        super().__init__()
+        self.class_weights = torch.tensor([0.1, 1.0, 1.0, 1.0, 1.0], dtype=torch.float32)
+        self.CELoss = nn.CrossEntropyLoss(weight=self.class_weights)
+        self.DiceCoef = DiceScore(num_classes=num_cls, include_background=False, average="macro", input_format="index")
+
+    def forward(self, pred, target):
+        ce_loss = self.CELoss(pred, target)
+        dice_loss = 1 - self.DiceCoef(pred, target)
+
+        return ce_loss + dice_loss
     
+
+def training(model, trainDL, optimizer, epoch, scheduler, device):
+    # 가중치 파일 저장 위치 정의
+    SAVE_PATH = './saved_models'
+    os.makedirs(SAVE_PATH, exist_ok=True)
+
+    # Early Stopping을 위한 변수
+    BREAK_CNT_LOSS = 0
+    BREAK_CNT_SCORE = 0
+    LIMIT_VALUE = 10
+
+    # Loss가 더 낮은 가중치 파일을 저장하기 위하여 Loss 로그를 담을 리스트
+    LOSS_HISTORY = []
+
+    for epoch in range(1, EPOCH + 1):
+        # GPU 환경에서 training과 testing을 반복하므로 eval 모드 -> train 모드로 전환
+        # testing에서는 train 모드 -> eval 모드
+        model.train()
+
+        SAVE_WEIGHT = os.path.join(SAVE_PATH, f"best_model_weights.pth")
+
+        loss_total = 0
+
+        # Train DataLoader에 저장된 Feature, Target 텐서로 학습 진행
+        for featureTS, targetTS in trainDL:
+            # GPU 환경으로 데이터 이동
+            featureTS = featureTS.to(device)
+            targetTS = targetTS.to(device)
+
+            # 결과 추론
+            pre_val = model(featureTS)
+
+            # 추론값으로 Loss값 계산
+            loss = CustomWeightedLoss(pre_val, targetTS)
+
+            # 활성화 함수 +
