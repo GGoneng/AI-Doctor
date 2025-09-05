b40b69dbdac1251974aaf53cc08b7b22612bcedf
GGoneng <zxzx0031@naver.com>
2025-09-05 21:06:19 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    Web/static/css/style.css
    	deleted:    Web/static/js/script.js
    	deleted:    Web/templates/main.html

--- Web/static/js/script.js
+++
@@ -1,8 +0,0 @@
-const plusBtn = document.getElementById("plusBtn");
-const plusMenu = document.getElementById("plusHidden");
-
-// + 버튼 토글 열기
-plusBtn.addEventListener("click", () => {
-    plusMenu.classList.toggle("hidden");
-});
-
