ab47eac5b88f806d3f76787fb3a063d233ac4c99
GGoneng <zxzx0031@naver.com>
2025-09-13 16:03:03 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Vision/Origin_UNet.py
    	modified:   Vision/model_test.py
    	modified:   frontend/Dockerfile.dev
    	new file:   frontend/postcss.config.js
    	modified:   frontend/public/index.html
    	modified:   frontend/src/App.js
    	modified:   frontend/src/index.css
    	new file:   frontend/tailwind.config.js

--- Vision/model_test.py
+++ Vision/model_test.py
@@ -7,6 +7,7 @@ from PIL import Image
 
 import albumentations as A
 
+import json
 import matplotlib.pyplot as plt
 
 WEIGHT_PATH = "./saved_models/best_model_weights.pth"
@@ -22,14 +23,43 @@ model.eval()
 
 transform = A.Compose([A.pytorch.ToTensorV2()])
 
-img_path = r"F:/Stomach_X-ray/Pediatric_Abdominal_X-ray/Validation/Source_Data/1.Pyloric_Stenosis/1_2000.png"
-img = Image.open(img_path).convert('L')
-img = np.array(img, dtype=np.float32) / 255.0
 
-augmented = transform(image = img)
+# 데이터 불러오기
+img_path = r"F:/Stomach_X-ray/Pediatric_Abdominal_X-ray/Validation/Source_Data/3.Air_Fluid_Level/3_1625.png"
+json_path = r"F:/Stomach_X-ray/Pediatric_Abdominal_X-ray/Validation/Labeling_Data/3.Air_Fluid_Level/3_1625.json"
+
+img = Image.open(img_path).convert('L')
+
+with open(json_path, "r", encoding="utf-8") as f:
+    item = json.load(f)
+
+mask = Image.new('L', img.size, 0)
+draw = ImageDraw.Draw(mask)
+
+
+for shape in item["shapes"]:
+    points = [tuple(point) for point in shape["points"]]
+    class_id = shape["class"]
+    draw.polygon(points, fill=class_id)
+
+img = np.array(img, dtype=np.float32) / 255.0
+mask = np.array(mask, dtype=np.int64)
+
+augmented = transform(image=img, mask=mask)
 img_tensor = augmented["image"].unsqueeze(0).to(DEVICE)
+mask_tensor = augmented["mask"]
 
 pred = model(img_tensor)
-pred = torch.argmax(F.softmax(pred, dim=1), dim=1)
+pred = torch.argmax(F.softmax(pred, dim=1), dim=1).cpu()
 
 print(pred.unique())
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 8))
+axes = axes.flatten()
+
+print(pred.shape, mask_tensor.shape)
+axes[0].imshow(pred.squeeze())
+axes[0].set_title("Prediction")
+axes[1].imshow(mask)
+axes[1].set_title("Mask")
+plt.show()
