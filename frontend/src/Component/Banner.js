7d3c2a5e05063382392fe9d736f6a9a9d03cc607
GGoneng <zxzx0031@naver.com>
2025-09-13 19:42:24 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    frontend/src/App.css
    	modified:   frontend/src/App.js
    	new file:   frontend/src/Component/Banner.js
    	new file:   frontend/src/Component/Header.js
    	deleted:    frontend/src/logo.svg

---
+++ frontend/src/Component/Banner.js
@@ -0,0 +1,16 @@
+import logo from "../Data/logo.png";
+
+function Banner() {
+    return (
+        <a href="http://localhost:3000/" className="flex justify-center">
+            <span>
+                <img src={logo} alt="AI-Doctor 로고" className="w-30 h-24"></img>
+            </span> 
+            <div className>
+
+            </div>
+        </a>
+    );
+}
+
+export default Banner;
