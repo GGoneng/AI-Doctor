a01b5d285cc16bec5b2e42403c13da26dc7e26dd
GGoneng <zxzx0031@naver.com>
2025-09-28 02:10:43 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   frontend/public/index.html
    	modified:   frontend/src/App.js
    	modified:   frontend/src/Component/Insert.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ import Insert from "./Component/Insert";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-full h-auto">
       <header className="flex">
         <div className="">
           <Header />
         </div>
       </header>
 
-      <main className="flex">
+      <main className="flex justify-center">
         <div>
           <Insert />
         </div>
--- frontend/src/Component/Insert.js
+++ frontend/src/Component/Insert.js
@@ -1,7 +1,7 @@
 function Insert() {
     return (
-        <div className="rounded-2xl bg-neutral-100/20">
-            <div>
+        <div className="flex items-center justify-center rounded-2xl bg-neutral-100/20 h-[500px] w-[848px]">
+            <div className=" h-[90%] w-[90%] border-2 border-transparent rounded-2xl hover:border-dotted hover:rounded-2xl hover:border-gray-400 p-4">
 
             </div>
         </div>
