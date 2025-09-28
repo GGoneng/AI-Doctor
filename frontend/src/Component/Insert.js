57a61d822b1af5803c1f3305b1b7cfbc086318d4
GGoneng <zxzx0031@naver.com>
2025-09-29 01:57:29 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   frontend/public/index.html
    	modified:   frontend/src/Component/Insert.js

--- frontend/src/Component/Insert.js
+++ frontend/src/Component/Insert.js
@@ -1,8 +1,18 @@
 function Insert() {
     return (
-        <div className="flex items-center justify-center rounded-2xl bg-neutral-100/20 h-[500px] w-[848px]">
-            <div className=" h-[90%] w-[90%] border-2 border-transparent rounded-2xl hover:border-dotted hover:rounded-2xl hover:border-gray-400 p-4">
-
+        <div className="flex items-center justify-center rounded-2xl bg-neutral-100/20 h-[350px] w-[848px] mt-20">
+            <div className="flex flex-col items-center justify-center h-[90%] w-[90%] border-2 border-transparent rounded-2xl hover:border-dotted hover:rounded-2xl hover:border-gray-400 p-4">
+                <h1 className="font-sans text-4xl font-bold">사진을 업로드해주세요</h1>
+                <div className="font-sans mt-10 w-[100%]"></div>
+                    <input className="hidden" area-hidden="true" tabindex="-1" type="file" accept=".jpg,.jpeg,.png,.heic,.heif,.webp,.svg"></input>
+                    <button className="flex items-center gap-2 px-4 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600" type="button">
+                        <span aria-hidden="true" className="w-5 h-5">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5">
+                                <path d="M5.734 8.438C6.008 5.64 8.387 3.5 11.208 3.5a5.5 5.5 0 0 1 5.422 4.562.24.24 0 0 0 .202.19c3.026.232 5.418 2.795 5.418 5.866 0 3.25-2.642 5.882-5.896 5.882H14.75a.75.75 0 0 1 0-1.5h1.604a4.389 4.389 0 0 0 4.396-4.382c0-2.287-1.788-4.199-4.033-4.371a1.738 1.738 0 0 1-1.565-1.433A4 4 0 0 0 11.208 5C9.151 5 7.425 6.563 7.227 8.584a1.69 1.69 0 0 1-1.096 1.42 4.384 4.384 0 0 0-2.881 4.114A4.389 4.389 0 0 0 7.646 18.5h.104A4.25 4.25 0 0 0 12 14.25v-.95L9.99 15.32a.75.75 0 1 1-1.063-1.059l3.276-3.29a.75.75 0 0 1 1.063 0l3.276 3.29a.75.75 0 1 1-1.063 1.059L13.5 13.33v.919A5.75 5.75 0 0 1 7.75 20h-.104c-3.254 0-5.896-2.631-5.896-5.882a5.884 5.884 0 0 1 3.865-5.523.191.191 0 0 0 .12-.157Z" fill="currentColor"/>
+                            </svg>
+                        </span>
+                        <span className="font-sans">이미지 업로드하기</span>
+                    </button>
             </div>
         </div>
     )
