21361f51631374a5002adb6dc98c4e56e824e01a
GGoneng <zxzx0031@naver.com>
2025-10-15 17:07:08 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   backend/result.png
    	modified:   frontend/src/Component/Loading.js

--- frontend/src/Component/Loading.js
+++ frontend/src/Component/Loading.js
@@ -1,6 +1,10 @@
+import Spinner from "../Data/spinner.gif";
+
 const Loading = () => {
     return (
-        <div>123</div>
+        <div className="w-full h-full">
+            <img src={Spinner} className="w-20 h-20 mt-20" />
+        </div>
     )
 }
 
