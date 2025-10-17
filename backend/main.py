0b22e597558c4e510f73ad3ad10395a9b2a73083
GGoneng <zxzx0031@naver.com>
2025-10-18 02:32:06 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   backend/main.py
    	modified:   backend/result.png

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from io import BytesIO
 
 import numpy as np
 
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 
 from uuid import uuid4
@@ -17,6 +17,7 @@ from uuid import uuid4
 import base64
 import redis
 import pickle
+import threading
 
 from typing import Optional
 
@@ -41,7 +42,8 @@ llm_memory = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379, d
 @app.post("/upload")
 async def upload_image(id: Optional[str] = Form(None),
                        file: Optional[UploadFile] = File(None), 
-                       text: Optional[str] = Form(None)):
+                       text: Optional[str] = Form(None),
+                       background_tasks: BackgroundTasks = None):
     
     if id is None:
         id = str(uuid4())
@@ -60,23 +62,36 @@ async def upload_image(id: Optional[str] = Form(None),
         llm_data = {}
 
 
-    if "imgs" not in vision_data:
-        vision_data["imgs"] = []
+    if "inputs" not in vision_data:
+        vision_data["inputs"] = []
     
-    if "texts" not in llm_data:
-        llm_data["texts"] = []
+    if "outputs" not in vision_data:
+        vision_data["outputs"] = []
+
+    if "inputs" not in llm_data:
+        llm_data["inputs"] = []
+
+    if "outputs" not in llm_data:
+        llm_data["outputs"] = []
     
+
     img = await file.read()
 
     if (len(img) == 0):
-        llm_data["texts"].append(text)
+        llm_data["inputs"].append(text)
+        background_tasks.add_task(predict_llm, id)
     
     elif not text:
-        vision_data["imgs"].append(img)
+        vision_data["inputs"].append(img)
+        background_tasks.add_task(predict_vision, id)
     
     else:
-        vision_data["imgs"].append(img)
-        llm_data["texts"].append(text)
+        vision_data["inputs"].append(img)
+        llm_data["inputs"].append(text)
+
+        # Vision 모델의 추론 우선 (속도, 증상 체크)
+        background_tasks.add_task(predict_vision, id)
+        # prediction_queue.append(("llm", id))
 
     vision_memory.set(id, pickle.dumps(vision_data))
     llm_memory.set(id, pickle.dumps(llm_data))
@@ -84,46 +99,52 @@ async def upload_image(id: Optional[str] = Form(None),
     return {"id": id, "file": file.filename, "prompt": text, "message": "업로드 성공!"}
 
 
-@app.post("/predictVision")
+gpu_lock = threading.Lock()
+
 def predict_vision(id: str):
-    data = pickle.loads(vision_memory.get(id))
-    img = data["img"]
-    img = Image.open(BytesIO(img)).convert("RGB")
+    with gpu_lock:
+        data = pickle.loads(vision_memory.get(id))
+        img = data["inputs"][-1]
+        img = Image.open(BytesIO(img)).convert("RGB")
 
-    img_np = np.array(img, dtype=np.float32)
-    num_classes = 5
+        img_np = np.array(img, dtype=np.float32)
+        num_classes = 5
 
-    pred = vision_predict(img=img, num_classes=num_classes, weights=VISION_WEIGHTS_PATH, device=DEVICE)
-    pred_np = pred.cpu().numpy()
+        pred = vision_predict(img=img, num_classes=num_classes, weights=VISION_WEIGHTS_PATH, device=DEVICE)
+        pred_np = pred.cpu().numpy()
 
-    symptom_list = ["증상 없음", "유문협착증", "기복증", "공기액체층", "변비"]
-    symptom_class = max(np.unique(pred_np))
+        symptom_list = ["증상 없음", "유문협착증", "기복증", "공기액체층", "변비"]
+        symptom_class = max(np.unique(pred_np))
 
-    symptom = symptom_list[symptom_class]
+        symptom = symptom_list[symptom_class]
 
-    print(symptom)
+        print(symptom)
 
-    palette = np.array([
-        [0, 0, 0],        # class 0 → black
-        [255, 0, 0],      # class 1 → red
-        [0, 255, 0],      # class 2 → green
-        [0, 0, 255],      # class 3 → blue
-        [255, 255, 0],    # class 4 → yellow
-    ], dtype=np.uint8)
+        palette = np.array([
+            [0, 0, 0],        # class 0 → black
+            [255, 0, 0],      # class 1 → red
+            [0, 255, 0],      # class 2 → green
+            [0, 0, 255],      # class 3 → blue
+            [255, 255, 0],    # class 4 → yellow
+        ], dtype=np.uint8)
 
-    color_mask = palette[pred_np] 
-    color_mask = color_mask.astype(np.float32)
+        color_mask = palette[pred_np] 
+        color_mask = color_mask.astype(np.float32)
 
-    blend_ratio = 0.3  # 투명도 (0.0~1.0)
-    pred_img = (img_np * (1 - blend_ratio) + color_mask * blend_ratio).astype(np.uint8)
+        blend_ratio = 0.3  # 투명도 (0.0~1.0)
+        pred_img = (img_np * (1 - blend_ratio) + color_mask * blend_ratio).astype(np.uint8)
 
-    buffer = BytesIO()
-    Image.fromarray(pred_img).save(buffer, format="PNG")
-    base64_img = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        buffer = BytesIO()
+        Image.fromarray(pred_img).save(buffer, format="PNG")
+        base64_img = base64.b64encode(buffer.getvalue()).decode("utf-8")
 
-    data["vision_pred"] = base64_img
-    vision_memory.set(id, pickle.dumps(data))
+        data["outputs"].append(base64_img)
+        vision_memory.set(id, pickle.dumps(data))
 
-    Image.fromarray(pred_img).save("result.png")
+        result_path = os.path.join(BASE_PATH, "result.png")
+        Image.fromarray(pred_img).save(result_path)
 
-    return {"id": id, "vision_result": "redis 저장 성공"}
+        return {"id": id, "vision_result": "redis 저장 성공"}
+
+def predict_llm(id: str):
+    return None
